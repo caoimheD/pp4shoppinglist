f8e7942eff8925e8a86619f5eb77ac4bea7be7bd
caoimheD <caoimhe.db5@gmail.com>
2022-12-01 15:01:39 +0000

    fix error in settings.py file

--- shoppinglist/settings.py
+++ shoppinglist/settings.py
@@ -82,12 +82,12 @@ WSGI_APPLICATION = 'shoppinglist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-#DATABASES = {
+# DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
-#}
+# }
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
