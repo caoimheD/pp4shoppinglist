14af9d72a5f0f86a3007607774d7c6634577a74f
caoimheD <caoimhe.db5@gmail.com>
2022-12-07 20:14:51 +0000

    create list model

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class List(models.Model):
+    title = models.CharField
+    slug = models.SlugField
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    featured_image = CloudinaryField('image')
+    excerpt = models.TextField(blank=True)
