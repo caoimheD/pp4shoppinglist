4d87bb3b6fa14d0823511855233107b147d810b6
caoimheD <caoimhe.db5@gmail.com>
2022-12-09 18:59:03 +0000

    create new model for items

--- shop/models.py
+++ shop/models.py
@@ -6,7 +6,7 @@ from cloudinary.models import CloudinaryField
 
 
 class List(models.Model):
-    title = models.CharField
+    title = models.CharField(max_length=50, null=False, blank=False, default=False)
     slug = models.SlugField
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     updated_on = models.DateTimeField(auto_now=True)
@@ -14,3 +14,11 @@ class List(models.Model):
     content = models.TextField()
     featured_image = CloudinaryField('image')
     excerpt = models.TextField(blank=True)
+
+
+class Item(models.Model):
+    name = models.CharField(null=False, blank=False, max_length=50)
+    done = models.BooleanField(null=False, blank=False, default=False)
+
+    def __str__(self):
+        return self.name
